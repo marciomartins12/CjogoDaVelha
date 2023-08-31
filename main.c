50d074eacc89a435dc3a5f45b907d4b5178e87f0
Marcio Martins <mmarciom1martins635@gmail.com>
2023-08-31 18:59:28 -0300

    .

--- main.c
+++ main.c
@@ -88,24 +88,41 @@ for(quantidadeDeJogadas = 0, vez = 0  ; quantidadeDeJogadas < 10 ; quantidadeDeJ
 	// vitoria em linha
 	if(	tralha[0][0]==playerDaVez && tralha[0][1]==playerDaVez && tralha[0][2]==playerDaVez){
 		vitoria = playerDaVez;
+		functionvitoria(playerDaVez);
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
+		quantidadeDeJogadas=10;
 	}else if(tralha[1][0]==playerDaVez && tralha[1][1]==playerDaVez && tralha[1][2]==playerDaVez){
+		quantidadeDeJogadas=10;
 		vitoria = playerDaVez;
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
 	}else if(tralha[2][0]==playerDaVez && tralha[2][1]==playerDaVez && tralha[2][2]==playerDaVez){
 		vitoria = playerDaVez;
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
+		quantidadeDeJogadas=10;
 	}
 	//vitoria em coluna
 	else if(tralha[0][0]==playerDaVez && tralha[1][0]==playerDaVez && tralha[2][0]==playerDaVez){
 		vitoria = playerDaVez;
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
+		quantidadeDeJogadas=10;
 	}else if(tralha[0][1]==playerDaVez && tralha[1][1]==playerDaVez && tralha[2][1]==playerDaVez){
 		vitoria = playerDaVez;
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
+		quantidadeDeJogadas=10;
 	}else if(tralha[0][2]==playerDaVez && tralha[1][2]==playerDaVez && tralha[2][2]==playerDaVez){
 		vitoria = playerDaVez;
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
+		quantidadeDeJogadas=10;
 	}
 	//vitoria vertical
 	else if(tralha[0][0]==playerDaVez && tralha[1][1]==playerDaVez && tralha[2][2]==playerDaVez){
 		vitoria = playerDaVez;
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
+		quantidadeDeJogadas=10;
 	}	else if(tralha[0][2]==playerDaVez && tralha[1][1]==playerDaVez && tralha[2][0]==playerDaVez){
 		vitoria = playerDaVez;
+		tabelaDoJogo( tralha[0][0], tralha[0][1], tralha[0][2],tralha[1][0], tralha[1][1], tralha[1][2],tralha[2][0], tralha[2][1], tralha[2][2]);
+		quantidadeDeJogadas=10;
 	}
 	
 	
@@ -122,7 +139,7 @@ for(quantidadeDeJogadas = 0, vez = 0  ; quantidadeDeJogadas < 10 ; quantidadeDeJ
 	
 }
 	
-	getchar();
+	
 	
 	printf("Quer jogar de novo? [s/sim n/não]");
 	scanf("%s", &parar);
@@ -131,5 +148,24 @@ for(quantidadeDeJogadas = 0, vez = 0  ; quantidadeDeJogadas < 10 ; quantidadeDeJ
 	system("cls");
 }while(parar != 'n');
 
+	return 0;
+}
+
+int tabelaDoJogo( a, b, c, d, e, f, g, h, i){
+	
+	printf("\t 1    2    3\n");
+	printf("\n");
+	printf("\t1  %c | %c  | %c\n", a, b, c);
+	printf("\t  ------------\n");
+	printf("\t2  %c | %c  | %c\n", d, e, f);
+	printf("\t  ------------\n");
+	printf("\t3  %c | %c  | %c\n", g, h, i);
+	
+	return 0;
+}
+
+int functionvitoria(jogador){
+	printf("O jogador %c venceu", jogador);
+	system("pause");
 	return 0;
 }
